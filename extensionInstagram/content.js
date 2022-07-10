6af87829985d1948c0162155cd75602a2e7e944e
yo <colacat1927@gmail.com>
2022-07-10 16:43:26 +0300

    поправил некоторые баги

---
+++ extensionInstagram/content.js
@@ -0,0 +1,210 @@
+window.onload = () => {
+  (async () => {
+    let isResult = await getData("getNow");
+    if (isResult) {
+      showResult();
+      return;
+    }
+    //фукнция для получения значений из нашего storage
+    function getData(sKey) {
+      return new Promise(function (resolve, reject) {
+        chrome.storage.local.get(sKey, function (items) {
+          if (chrome.runtime.lastError) {
+            console.error(chrome.runtime.lastError.message);
+            reject(chrome.runtime.lastError.message);
+          } else {
+            resolve(items[sKey]);
+          }
+        });
+      });
+    }
+
+    async function showResult() {
+      let body = document.getElementsByTagName("body")[0];
+      let result = await getData("savedPosts");
+      new Promise((resolve, reject) => {
+        body.innerHTML = "";
+        resolve();
+      })
+        .then(() => {
+          result.forEach((item) => {
+            const p = document.createElement("p");
+            p.textContent = item;
+            body.append(p);
+          });
+        })
+        .then(() => {
+          chrome.storage.local.set({ savedPosts: [], getNow: false });
+        });
+    }
+
+    function reset() {
+      chrome.storage.local.set({ posts: [] });
+      chrome.storage.local.set({ savedPosts: [] });
+      chrome.storage.local.set({ scraperCounter: 0 });
+    }
+
+    function sleep(ms) {
+      return new Promise((resolve) => setTimeout(resolve, ms));
+    }
+
+    //obeserver для сброса значений при переходе на другую страницу
+    var previousUrl = "";
+
+    let isWorking = await getData("isWorking");
+    var scraperCounter = parseInt(await getData("scraperCounter"));
+
+    var observer = new MutationObserver(async function (mutations) {
+      isWorking = await getData("isWorking");
+
+      if (location.href !== previousUrl && !isWorking) {
+        previousUrl = location.href;
+        reset(); //обнуляем значения в storage
+      }
+    });
+
+    const config = { subtree: true, childList: true };
+    observer.observe(document, config);
+
+    if (!isWorking) {
+      console.log("Расширение Instagram работает");
+    }
+
+    try {
+      if (isWorking) {
+        let posts = await getData("posts");
+        console.log(`Номер текущего поста: ${scraperCounter + 1}`);
+        if (scraperCounter < posts.length) {
+          getPhotos();
+        }
+      }
+    } catch {}
+
+    window.addEventListener("scroll", () => {
+      getCount();
+    });
+
+    async function getCount() {
+      let links = document.querySelectorAll("a[href*=p/*]");
+      let posts = await getData("posts");
+      for (let i = 0; i < links.length; i++) {
+        if (!posts.includes(links[i].href)) {
+          if (
+            links[i].href.includes("https://www.instagram.com/p/") &&
+            !/(comments|liked_by)/.test(links[i])
+          ) {
+            posts.push(links[i].href);
+            chrome.storage.local.set({ posts: posts });
+          }
+        }
+      }
+    }
+
+    var innerPosts = [];
+
+    function getPhotos() {
+      new Promise(async (resolve, reject) => {
+        let isExist = document.querySelector("button._aahi"),
+          counter = 0,
+          allPhotos = document.querySelectorAll("article	img._aagt"),
+          allVideos = document.querySelectorAll("article	video"),
+          awaitElem = 0;
+
+        try {
+          while (
+            allPhotos[0] === undefined &&
+            allVideos[0] === undefined &&
+            awaitElem < 40
+          ) {
+            awaitElem += 1;
+            allPhotos = document.querySelectorAll("article	img._aagt");
+            allVideos = document.querySelectorAll("article	video");
+            await sleep(150);
+          }
+        } catch {}
+
+        try {
+          for (let i = 0; i < allPhotos.length; i++) {
+            if (!innerPosts.includes(allPhotos[i].src)) {
+              innerPosts.push(allPhotos[i].src);
+              allPhotos[i].style.filter = "grayscale(100%)";
+            }
+          }
+        } catch {}
+
+        try {
+          for (let i = 0; i < allVideos.length; i++) {
+            if (!innerPosts.includes(allVideos[i].src)) {
+              innerPosts.push(allVideos[i].src);
+              allVideos[i].style.filter = "grayscale(100%)";
+            }
+          }
+        } catch {}
+
+        while (counter < 25 && !isExist) {
+          counter += 1;
+          // console.log("Попытка найти еще фотографии или видео: " + counter);
+          // console.log("Сон 1 секунда");
+          await sleep(100);
+          isExist = document.querySelector("button._aahi");
+        }
+        if (isExist) {
+          // console.log("Сохраняю фото/видео");
+          resolve(isExist);
+        } else {
+          // console.log("На странице больше нету фото/видео");
+          reject(innerPosts);
+        }
+      })
+        .then(async (response) => {
+          await sleep(100);
+          response.click();
+          await sleep(200);
+          await getPhotos();
+        })
+        .catch(async () => {
+          // console.log("Фото/Видео в списке больше нету");
+          // console.log(`Количество полученных фото/видео: ${photos.length}`);
+          try {
+            let scraperCounter = await getData("scraperCounter");
+            scraperCounter += 1;
+            chrome.storage.local.set({
+              scraperCounter: scraperCounter,
+            });
+
+            let savedPosts = await getData("savedPosts");
+            savedPosts = savedPosts.concat(innerPosts);
+
+            let filtered = [];
+            for (let j = 0; j < savedPosts.length; j++) {
+              if (!filtered.includes(savedPosts[j])) {
+                filtered.push(savedPosts[j]);
+              }
+            }
+
+            chrome.storage.local.set({
+              savedPosts: filtered,
+            });
+
+            let posts = await getData("posts");
+            let getNow = await getData("getNow");
+
+            if (scraperCounter < posts.length) {
+              window.location.href = posts[scraperCounter];
+            } else {
+              if (isWorking || getNow) {
+                showResult();
+                chrome.storage.local.set({
+                  isWorking: false,
+                  posts: [],
+                  scraperCounter: 0,
+                  getNow: false,
+                });
+                return;
+              }
+            }
+          } catch {}
+        });
+    }
+  })();
+};
---
+++ extensionInstagram/popup.js
@@ -0,0 +1,133 @@
+function getData(sKey) {
+  return new Promise(function (resolve, reject) {
+    chrome.storage.local.get(sKey, function (items) {
+      if (chrome.runtime.lastError) {
+        console.error(chrome.runtime.lastError.message);
+        reject(chrome.runtime.lastError.message);
+      } else {
+        resolve(items[sKey]);
+      }
+    });
+  });
+}
+
+function loadUrl(links, counter) {
+  chrome.tabs.update({ url: links[counter] });
+}
+
+document.addEventListener("DOMContentLoaded", async function () {
+  const getPostsButton = document.getElementById("getPosts");
+  const stopCollectButton = document.getElementById("stopCollect");
+  const postsLength = document.getElementById("postsLength");
+  const postsSaved = document.getElementById("postsSaved");
+  const extensionActive = document.getElementById("ok");
+  const extensionHidden = document.getElementById("error");
+  const instagramButton = document.getElementById("instagram");
+  const download = document.getElementById("settings");
+  let alert = document.querySelector(".alert");
+
+  //установка актуальных значения в popup
+  await getData("posts").then((posts) => {
+    postsLength.textContent = posts.length;
+  });
+
+  await getData("savedPosts").then((savedPosts) => {
+    postsSaved.textContent = savedPosts.length || 0;
+  });
+
+  await getData("isWorking").then((isWorking) => {
+    if (isWorking) {
+      getPostsButton.setAttribute("disabled", true);
+      stopCollectButton.removeAttribute("disabled");
+    } else {
+      stopCollectButton.setAttribute("disabled", true);
+      getPostsButton.removeAttribute("disabled");
+    }
+  });
+
+  chrome.storage.onChanged.addListener(function (changes, namespace) {
+    if (changes.posts) {
+      postsLength.textContent = changes.posts.newValue.length; //установка актуального значения для количества собранных постов
+    }
+    if (changes.savedPosts) {
+      postsSaved.textContent = changes.savedPosts.newValue.length; //установка актуального значения для сохраненных внутренних постов
+    }
+  });
+
+  //----------------------------------------
+
+  getPostsButton.addEventListener("click", async () => {
+    let isWorking = await getData("isWorking");
+
+    let posts = await getData("posts");
+
+    if (!isWorking) {
+      let scraperCounter = (await getData("scraperCounter")) || 0;
+      if (posts.length && scraperCounter < posts.length) {
+        chrome.storage.local.set({ isWorking: true, getNow: false });
+
+        getPostsButton.setAttribute("disabled", "true");
+        stopCollectButton.removeAttribute("disabled");
+        alert.textContent = "Расширение собирает посты";
+        alert.classList.remove("hidden");
+
+        loadUrl(posts, scraperCounter);
+      } else {
+        alert.textContent = "Вы еще не собрали ссылки на посты";
+        alert.classList.remove("hidden");
+      }
+    } else {
+      alert.textContent = "Расширение уже собирает посты";
+      alert.classList.remove("hidden");
+    }
+  });
+
+  instagramButton.addEventListener("click", () => {
+    chrome.tabs.update({ url: "https://www.instagram.com" });
+    extensionActive.classList.remove("hidden");
+    extensionHidden.classList.add("hidden");
+  });
+
+  stopCollectButton.addEventListener("click", async () => {
+    let isWorking = await getData("isWorking");
+    if (isWorking) {
+      alert.textContent = "Останавливаю сбор постов"; //Сюда вернуть ответ о том что расширение полностью остановлено
+      alert.classList.remove("hidden");
+      stopCollectButton.setAttribute("disabled", "true");
+      getPostsButton.removeAttribute("disabled");
+      chrome.storage.local.set({ isWorking: false, getNow: false });
+    }
+  });
+
+  download.addEventListener("click", async () => {
+    let savedPosts = await getData("savedPosts");
+    if (savedPosts) {
+      chrome.storage.local.set({
+        isWorking: false,
+        posts: [],
+        scraperCounter: 0,
+        getNow: true,
+      });
+      savedPosts.textContent = "0";
+      alert.textContent = "Сохраняю результат";
+      if (alert.classList.contains("hidden")) {
+        alert.classList.remove("hidden");
+      }
+    }
+  });
+
+  chrome.tabs.query({}, (tabs) => {
+    tabs.forEach((tab) => {
+      if (tab.active) {
+        if (tab.url.includes("instagram.com")) {
+          extensionActive.classList.remove("hidden");
+          extensionHidden.classList.add("hidden");
+          chrome.tabs.sendMessage(tab.id, { type: "GET_COUNT" });
+        } else {
+          extensionActive.classList.add("hidden");
+          extensionHidden.classList.remove("hidden");
+        }
+      }
+    });
+  });
+});
